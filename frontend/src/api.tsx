87368f4d8e42ae1da66fae87f1ac992dee361f22
andrewpfs <152920778+andrewpfs@users.noreply.github.com>
2025-12-09 20:14:31 -0600

    added type safety to fetch results

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -2,19 +2,14 @@ import { CompanySearch } from "./company";
 
 interface SearchResponse {
   data: CompanySearch[];
-  
 }
 
-export const searchCompanies = async (query : string) => {
-  try{
-    const res = await fetch(`https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_API_KEY}`) 
+export const searchCompanies = async (query : string): Promise<CompanySearch[]> => {
+    const res = await fetch(`https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`) 
     if(!res.ok){
       throw new Error(`Response status: ${res.status}`)
     }
     const result = await res.json()
     console.log(result)
     return result
-  }catch(error){
-    console.error(error)
-  }
 }
